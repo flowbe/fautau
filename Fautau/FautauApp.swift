a0e9df68ca0ad9b307e4ee0e1518974db9c5e283
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-06-14 09:12:02 +0200

    Initial Commit

---
+++ Fautau/FautauApp.swift
@@ -0,0 +1,17 @@
+//
+//  FautauApp.swift
+//  Fautau
+//
+//  Created by Florentin BEKIER on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FautauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
